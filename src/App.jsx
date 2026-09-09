a3154e6bf501f9c74547addb7ae4f76c5220dc05
Luana Miyashiro <152427133+miyamiyashiro@users.noreply.github.com>
2026-09-08 21:32:49 -0300

    refactor: render tab content from array

--- src/App.jsx
+++ src/App.jsx
@@ -48,10 +48,9 @@ export default function App() {
 
         <div id="tab-content">
           <ul>
-            <li>React is extremely popular</li>
-            <li>It makes building complex, interactive UIs a breeze</li>
-            <li>It's powerful & flexible</li>
-            <li>It has a very active and versatile ecosystem</li>
+            {content[0].map((item)=>(
+               <li key={item}>{item}</li>
+            ))}
           </ul>
         </div>
       </div>
